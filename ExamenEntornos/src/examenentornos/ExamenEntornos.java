915363bd53fc5a80311e5eee1068ec456a526b99
Victor <Victor@192.168.56.1>
2016-03-11 11:38:20 +0100

    Segundo COMMIT del Examen.

--- ExamenEntornos/src/examenentornos/ExamenEntornos.java
+++ ExamenEntornos/src/examenentornos/ExamenEntornos.java
@@ -15,7 +15,7 @@ public class ExamenEntornos {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    System.out.println("Hago un cambio en el proyecto para el examen");
   }
   
 }
